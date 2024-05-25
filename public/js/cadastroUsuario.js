dd84c45998f616988da175efd929ab096f6386c4
nathannolacio <nathannolacio04@gmail.com>
2024-05-25 17:48:36 -0300

    :sparkles::lipstick: feat: Ajustes na página de cadastro - adicionado componentes

--- public/js/cadastroUsuario.js
+++ public/js/cadastroUsuario.js
@@ -2,11 +2,15 @@ function cadastrarUsuario() {
     const nome = ipt_nome.value
     const email = ipt_email.value
     const senha = ipt_senha.value
-    // const confirmacaoSenha = ipt_confirmacaoSenha.value
+    const confirmacaoSenha = ipt_confirmacaoSenha.value
+    const curso = ipt_curso.value
+    const nivel = ipt_nivel.value
 
     if (nome == '' || email == '' || senha == '') {
-        mensagem_erro.innerHTML = 'Mensagem de erro para campos em branco'
-    }
+        mensagem_erro.innerHTML = 'Todos os campos devem ser preenchidos!'
+    } else if (senha != confirmacaoSenha) {
+        mensagem_erro.innerHTML = 'As senhas informadas não são iguais!'
+    } else
 
     fetch("/usuarios/cadastrar", {
         method: "POST",
@@ -19,4 +23,18 @@ function cadastrarUsuario() {
             senhaServer: senha
         }),
     })
+}
+
+function irParaPaginaInicial() {
+    window.location = 'index.html'
+}
+
+function irParaCadastroParte2() {
+    cadastro_pt1.style.display = 'none'
+    cadastro_pt2.style.display = 'flex'
+}
+
+function irParaPaginaCadastro() {
+    cadastro_pt2.style.display = 'none'
+    cadastro_pt1.style.display = 'flex'
 }
