9267c504bddbd608083a6c75aa890583e4366fd3
nathannolacio <nathannolacio04@gmail.com>
2024-06-09 23:18:59 -0300

    :bug: fix: Ajustes na página de início e na página home

--- public/js/home.js
+++ public/js/home.js
@@ -1,154 +1,26 @@
-window.onload = verificarUsuario()
+const conteudoContainer = document.querySelector('.conteudo-container')
+const cardConteudo = document.querySelectorAll('.card-conteudo')
+const tituloConteudo = document.querySelector('.titulo-conteudo')
+const btnFecharModal = document.querySelector('.fa-xmark')
 
-function verificarUsuario() {
-    const idUsuario = sessionStorage.ID_USUARIO
-    console.log(idUsuario)
+import { conteudos } from "../conteudos/conteudos.js"
 
-    fetch("/matriculas/listarPorIdUsuario", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify({
-            idUsuario: idUsuario
-        })
-    })
-    .then(function (resposta) {
-        if(resposta.ok) {
-            console.log(resposta)
+let idConteudo = 0
 
-            resposta.json().then(json => {
-                console.log('exibindo o resultado aqui em baixo:')
-                console.log(json)
-                console.log(JSON.stringify(json))
-
-                if(json.length == 0) {
-                    validarSessao()
-                    modal_matricula.style.display = 'flex'
-                }
-            })
-        }
-    })
-    .catch(function (erro) {
-        console.log(erro)
-    })
-
-    // listarProximasAulas(idUsuario)
-
-    return false
-}
-
-function abrirModalAgendarAula() {
-    modal_agendar_aula.style.display = 'flex'
-}
-
-function fecharModalAgendarAula() {
-    div_data_aula.style.display = 'block'
-    div_horario_aula.style.display = 'block'
-    div_professor_aula.style.display = 'none'
-    btn_agendar.style.display = 'none'
-    btn_prox_etapa.style.display = 'block'
-
-    ipt_professor.innerHTML = `
-    <option value=''>Selecione um(a) professor(a)</option>
-    `
-
-    ipt_data.value = ''
-    ipt_horario.value = ''
-
-    modal_agendar_aula.style.display = 'none'
-}
-
-function proximaEtapaAgendamento() {
-    div_data_aula.style.display = 'none'
-    div_horario_aula.style.display = 'none'
-    div_professor_aula.style.display = 'block'
-    btn_prox_etapa.style.display = 'none'
-    btn_agendar.style.display = 'block'
-    
-    listarProfessores()
-}
-
-function listarProfessores() {
-    fetch("/professores/listar", {
-        method: 'GET'
-    })
-    .then(function (resposta) {
-        resposta.json().then((professores) => {
-            professores.forEach((professor) => {
-                ipt_professor.innerHTML += `
-                <option value='${professor.idProfessor}'>${professor.nome}</option>
-                `
-            })
-        })
-    })
-    .catch(function (resposta) {
-        console.log(`#ERRO: ${resposta}`)
+for (let item = 0; item < cardConteudo.length; item++) {
+    cardConteudo[item].addEventListener("click", function (evento) {
+        idConteudo = this.getAttribute('idConteudo')
+        abrirModalTema()
     })
 }
 
-function formatarData(dataAula) {
-    const data = new Date(dataAula)
-    const dataFormatada = (data.getFullYear() + '-' + ((data.getMonth() + 1)) + '-' + (data.getDate() + 1))
+function abrirModalTema() {
+    modal_tema.style.display = 'flex'
 
-    return dataFormatada
+    tituloConteudo.innerHTML = conteudos[idConteudo-1].titulo
+    conteudoContainer.innerHTML = conteudos[idConteudo-1].conteudo
 }
 
-function agendarAula() {
-    const dataAula = ipt_data.value
-    const horarioAula = ipt_horario.value
-    const idUsuario = sessionStorage.ID_USUARIO
-    const idProfessor = ipt_professor.value
-    const dataFormatada = formatarData(dataAula)
-
-    fetch("/aulas/cadastrar", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify({
-            idUsuario: idUsuario,
-            idProfessor: idProfessor,
-            data: dataFormatada,
-            horario: horarioAula
-        })
-    })
-    .then(function (resposta) {
-        console.log("resposta: ", resposta)
-
-        if(resposta.ok) {
-            fecharModalAgendarAula()
-        } else {
-            throw "Houve um erro ao tentar agendar a aula!"
-        }
-    })
-    .catch(function (resposta) {
-        console.log(`#ERRO: ${resposta}`)
-    })
-
-    return false
+btnFecharModal.onclick = function fecharModalTema() {
+    modal_tema.style.display = 'none'
 }
-
-// function listarProximasAulas(idUsuario) {
-//     fetch("/aulas/listarProximasAulas", {
-//         method: "POST",
-//         headers: {
-//             "Content-Type": "application/json"
-//         },
-//         body: JSON.stringify({
-//             idUsuario: idUsuario
-//         })
-//     })
-//     .then(function (resposta) {
-//         console.log("resposta: ", resposta)
-        
-//         if(!resposta.ok) {
-//             throw "Houve um erro ao tentar listar a próxima aula!"
-//         }
-//     })
-//     .catch(function (resposta) {
-//         console.log(`#ERRO: ${resposta}`)
-//     })
-
-//     return false
-// }
