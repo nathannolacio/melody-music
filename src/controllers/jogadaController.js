3f534b68afacc9d54e8e649dcab7676292603e60
nathannolacio <nathannolacio04@gmail.com>
2024-06-10 02:22:39 -0300

    :bug: fix: Ajustes no quiz

--- src/controllers/jogadaController.js
+++ src/controllers/jogadaController.js
@@ -3,17 +3,23 @@ var jogadaModel = require("../models/jogadaModel");
 function cadastrar(req, res) {
     var idUsuario = req.body.idUsuario;
     var idQuiz = req.body.idQuiz;
+    var pontuacao = req.body.pontuacao;
     var qtdAcertos = req.body.qtdAcertos;
+    var qtdErros = req.body.qtdErros;
 
     if (idUsuario == undefined) {
         res.status(400).send("O idUsuario está undefined!");
     } else if (idQuiz == undefined) {
         res.status(400).send("O idQuiz está undefined!");
+    } else if (pontuacao == undefined) {
+        res.status(400).send("A pontuacao está undefined!");
     } else if (qtdAcertos == undefined) {
         res.status(400).send("A qtdAcertos está undefined!");
+    } else if (qtdErros == undefined) {
+        res.status(400).send("A qtdErros está undefined!");
     } else {
 
-        jogadaModel.cadastrar(idUsuario, idQuiz, qtdAcertos)
+        jogadaModel.cadastrar(idUsuario, idQuiz, pontuacao, qtdAcertos, qtdErros)
             .then(function(resposta){
                 res.status(200).send("Cadastro realizado com sucesso")
             }).catch(function(erro){
--- src/models/jogadaModel.js
+++ src/models/jogadaModel.js
@@ -1,9 +1,9 @@
 var database = require("../database/config")
 
-function cadastrar(idUsuario, idQuiz, qtdAcertos) {
+function cadastrar(idUsuario, idQuiz, pontuacao, qtdAcertos, qtdErros) {
     var instrucaoSql = `
-        insert into jogada(fkUsuario, fkQuiz, pontuacao) values
-        (${idUsuario}, ${idQuiz}, ${qtdAcertos});
+        insert into jogada(fkUsuario, fkQuiz, pontuacao, acertos, erros) values
+        (${idUsuario}, ${idQuiz}, ${pontuacao}, ${qtdAcertos}, ${qtdErros});
     `;
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
